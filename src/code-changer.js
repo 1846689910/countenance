b5e602db81a13bd361078f98c5b992c3bb8d3fea
1846689910 <zuhe75@gmail.com>
2019-03-28 09:03:03 -0700

    update repo way

--- src/code-changer.js
+++ src/code-changer.js
@@ -1,6 +1,7 @@
 const Promise = require("bluebird");
+const Fs = require("fs");
 const { decode, encode } = require("confi-coder/src/coder");
-const repositories = require("./repositories");
+const repositories = require("./repositories.json");
 const { getDecodedPath, getEncodedPath } = require("./utils");
 const shell = require("shelljs");
 const exec = Promise.promisify(shell.exec);
@@ -11,17 +12,19 @@ const workingBranch = "update";
 const noPush = process.argv.find(x => x === "--np");
 
 async function main() {
+  const freshCode = await getFreshCode();
   const repos = repositories && Object.entries(repositories);
   if (repos && repos.length > 0) {
     await newBranch();
     for (let i = 0, len = repos.length; i < len; i++) {
       const [k, repo] = repos[i];
       const { name, code } = repo;
-      await decode(getEncodedPath(`${name}.ecd`), getDecodedPath(name), code.before);
-      await encode(getDecodedPath(name), getEncodedPath(`${name}.ecd`), code.next);
+      await decode(getEncodedPath(`${name}.ecd`), getDecodedPath(name), code);
+      await encode(getDecodedPath(name), getEncodedPath(`${name}.ecd`), freshCode);
       await track();
     }
     if (!noPush) await exec(`git push origin ${workingBranch}`);
+    resetRepoCode();
   }
 }
 
@@ -33,8 +36,25 @@ async function newBranch() {
   await exec(commands("git checkout master", `git checkout -b ${workingBranch}`));
 }
 
+async function getFreshCode(){
+  const arg = process.argv.find(x => x.includes("--code="));
+  if (!arg) {
+    throw new Error("fresh code is required use `node src/code-changer.js --code=FRESH_CODE`")
+  } else {
+    return arg.split("=")[1];
+  }
+}
+
+async function resetRepoCode(fresh){
+  for (const key in repositories) {
+    const repo = repositories[key];
+    repo.code.before = fresh;
+  }
+  Fs.writeFileSync("./repositories.json", JSON.stringify(repositories, null, 2));
+}
+
 function commands(...args) {
   return args.join(` ${sep} `);
 }
 
-main();
+// main();
--- src/encode-handler.js
+++ src/encode-handler.js
@@ -1,7 +1,7 @@
-const repositories = require("./repositories");
+const repositories = require("./repositories.json");
 const { encode } = require("confi-coder/src/coder");
 const { getDecodedPath, getEncodedPath } = require("./utils");
 
 Object.entries(repositories).forEach(([k, repo]) =>
-  encode(getDecodedPath(repo.name), getEncodedPath(`${repo.name}.ecd`), repo.code.before)
+  encode(getDecodedPath(repo.name), getEncodedPath(`${repo.name}.ecd`), repo.code)
 );
